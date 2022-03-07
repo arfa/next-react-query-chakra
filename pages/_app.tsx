0e39dcf4056d66e697a07cf620bc2f4f15182ba9
Marwen Arfa <marwen.arfa@gmail.com>
2022-03-07 06:29:52 +0100

    chore: setup chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/global.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
